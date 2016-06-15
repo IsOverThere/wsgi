18ebc8ef1e1fda6132d05ff4918d0d0d50e29441
zhangcl <chengliang.zhang@technicolor.com>
2016-06-15 14:45:50 +0800

    ls | sort

--- rate_info.py
+++ rate_info.py
@@ -9,7 +9,7 @@ def show_current_status(environ, start_response):
     return output 
 
 def show_list(environ, start_response):
-    status, output = commands.getstatusoutput('ls -als /var/log/iptables.log')
+    status, output = commands.getstatusoutput('ls /var/log/iptables.log|sort')
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
