85ea023e2a6e7308d8e0a3cc71d39720a1b56d39
zhangcl <chengliang.zhang@technicolor.com>
2016-06-16 18:43:45 +0800

    added css style

--- rate_info.py
+++ rate_info.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python
 import commands
 import re
+style = """
+<style type="text/css">
+a:link {
+    font-size: 10pt;
+    color: #006699;
+    font-family: Calibri;
+    text-align: left;
+    text-decoration: none;
+}
+a:visited {
+    font-size: 10pt;
+    color: #990066;
+    font-family: Calibri;
+    text-align: left;
+    text-decoration: none;
+}
+a:hover {
+    font-size: 10pt;
+    color: #cccc33;
+    font-family: Calibri;
+    text-align: left;
+    text-decoration: underline;
+}
+a:active {
+    font-size: 10pt;
+    color: #ff6666;
+    font-family: Calibri;
+    text-align: left;
+    text-decoration: underline;
+}
+</style>
+
+"""
 
 html = """ 
 <html>
+    <head>
+    %s
+    </head>
     <body>
     %s
     </body>
 </html>
 """
  
+
 def current_status(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
@@ -19,24 +56,26 @@ def current_status(environ, start_response):
     return output 
 
 
-def get_log_list(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/html')]
-    start_response(status, response_headers)
-
+def get_log_list_html():
     status, list = commands.getstatusoutput('ls /var/log/iptables.log')
     list = list.split()
     body=''
     for line in list:
         body = body+'\t<a href="list.wsgi?file='+line+'">'+line+'</a><br>\n'
-    output=html%(body)
-    return output 
+    output=html%(style, body)
+    return output
 
-def get_one_log(environ, start_response):
+
+def get_log_list(environ, start_response):
     status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
+    response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
 
+    output=get_log_list_html()
+    return output 
+
+
+def get_one_log_html(environ):
     query_string = environ['QUERY_STRING']
     fileMatch = re.match('^file=(.*)$', query_string, re.M|re.I)
     if fileMatch:
@@ -46,6 +85,16 @@ def get_one_log(environ, start_response):
     logfile = '/var/log/iptables.log/'+logfile
     status, output = commands.getstatusoutput('cat ' + logfile )
     return output 
+
+
+def get_one_log(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+
+    output = get_one_log_html(environ)
+    return output 
+
 
 def list(environ, start_response):
     query_string = environ['QUERY_STRING']
@@ -60,3 +109,7 @@ def list(environ, start_response):
 #response_headers = [('Content-type', 'text/plain')]
 #start_response(status, response_headers)
 #return output 
+
+if __name__ == '__main__':
+    output=get_log_list_html()
+    status, output = commands.getstatusoutput('echo "' + output + '" > /home/zhangcl/test.htm')
